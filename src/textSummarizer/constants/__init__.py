4d75e9c016bc076e9d8ae7a1a3819161c4d9fb24
RITIK RAJ <ritikvashisth1@gmail.com>
2025-06-22 00:11:00 +0530

    data ingestion notebook added

---
+++ src/textSummarizer/constants/__init__.py
@@ -0,0 +1,10 @@
+# from pathlib import Path
+
+# CONFIG_FILE_PATH = Path("config/config.yaml")
+# PARAMS_FILE_PATH = Path("params.yaml")
+from pathlib import Path
+
+CONFIG_FILE_PATH = Path("config/config.yaml")
+PARAMS_FILE_PATH = Path("params.yaml")
+
+# Check if the configuration file exists
